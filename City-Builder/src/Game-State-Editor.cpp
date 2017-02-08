dd6a5494f4b7e1bb729aab9273b55535067327e9
Kevin <kjd2060@rit.edu>
2017-02-08 10:43:36 -0500

    Added ability to pan map and zoom

--- City-Builder/src/Game-State-Editor.cpp
+++ City-Builder/src/Game-State-Editor.cpp
@@ -39,11 +39,59 @@ void GameStateEditor::handleInput()
 		case sf::Event::Resized:
 		{
 			gameView.setSize(event.size.width, event.size.height);
+			gameView.zoom(zoomLevel);
 			guiView.setSize(event.size.width, event.size.height);
 			this->game->background.setPosition(this->game->window.mapPixelToCoords(sf::Vector2i(0, 0), this->guiView));
 			this->game->background.scale(
 				float(event.size.width) / float(this->game->background.getTexture()->getSize().x),
 				float(event.size.height) / float(this->game->background.getTexture()->getSize().y));
+			break;
+		}
+		case sf::Event::MouseMoved:
+		{
+			// pan the camera
+			if (this->actionState == ActionState::PANNING)
+			{
+				sf::Vector2f pos = sf::Vector2f(sf::Mouse::getPosition(this->game->window) - this->panningAnchor);
+				gameView.move(-1.0f * pos * this->zoomLevel);
+				panningAnchor = sf::Mouse::getPosition(this->game->window);
+			}
+			break;
+		}
+		case sf::Event::MouseButtonPressed:
+		{
+			// start panning
+			if (event.mouseButton.button == sf::Mouse::Middle)
+			{
+				if (this->actionState != ActionState::PANNING)
+				{
+					this->actionState = ActionState::PANNING;
+					this->panningAnchor = sf::Mouse::getPosition(this->game->window);
+				}
+			}
+			break;
+		}
+		case sf::Event::MouseButtonReleased:
+		{
+			// stop panning
+			if (event.mouseButton.button == sf::Mouse::Middle)
+			{
+				this->actionState = ActionState::NONE;
+			}
+			break;
+		}
+		case sf::Event::MouseWheelMoved:
+		{
+			if (event.mouseWheel.delta < 0)
+			{
+				gameView.zoom(2.0f);
+				zoomLevel *= 2.0f;
+			}
+			else
+			{
+				gameView.zoom(0.5f);
+				zoomLevel *= 0.5f;
+			}
 			break;
 		}
 		default:break;
